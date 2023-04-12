afd37b5c4ffb0dc2fe9b9fe9b8a411d43b183326
yang yunfeng <63218554+yangyunfeng-cyber@users.noreply.github.com>
2023-04-12 10:04:43 +0800

    Create image_normalize.py

---
+++ image_normalize.py
@@ -0,0 +1,18 @@
+def file_name(file_dir):
+    """循环目录，将MR的灰度值归一化到[0，255]"""
+    for root, dirs, files in os.walk(file_dir):     
+        for path in dirs:
+            end_dirs = root +'/' + path
+            # 对image进行重采样
+            if 'T2' in end_dirs:
+                imageName = end_dirs +'/Pre_N4_resam113_MR.nii'                                 
+                image= sitk.ReadImage(imageName)               
+                resacleFilter = sitk.RescaleIntensityImageFilter()
+                resacleFilter.SetOutputMaximum(255)
+                resacleFilter.SetOutputMinimum(0)
+                image = resacleFilter.Execute(image)
+                sitk.WriteImage(image, end_dirs+'/Pre_N4_resam113_norm_MR.nii') 
+                print(imageName)             
+
+file_name('A:/BaiduNetdiskDownload/tu xiang-zhao')    
+
