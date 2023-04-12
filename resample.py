3fc2c98d89461da248eb8bcf1fb61efa67904d8e
yang yunfeng <63218554+yangyunfeng-cyber@users.noreply.github.com>
2023-04-12 09:57:10 +0800

    Create resample.py

---
+++ resample.py
@@ -0,0 +1,28 @@
+def file_name(file_dir):
+    """循环目录，对MR进行重采样规范化"""
+    for root, dirs, files in os.walk(file_dir):     
+        for path in dirs:
+            end_dirs = root +'/' + path
+            # 对image进行重采样
+            if 'T2' in end_dirs:
+                imageName = end_dirs +'/Pre_N4_MR.nii'                                   
+                print(imageName)   
+                newSpacing = [1,1,3]  #可根据数据本身的特点来确定，尽量不过多改变数据
+                newSpacing = np.array(newSpacing, float)           
+                img = sitk.ReadImage(imageName)   
+                newSize = img.GetSize()* img.GetSpacing() / newSpacing 
+                newSize = newSize.astype(np.int_)
+
+                resample = sitk.ResampleImageFilter()
+                resample.SetSize(newSize.tolist())
+                resample.SetOutputSpacing(newSpacing)
+                resample.SetInterpolator(sitk.sitkBSpline)
+                resample.SetOutputDirection(img.GetDirection())
+                resample.SetOutputOrigin(img.GetOrigin())      
+                resample.SetTransform(sitk.Transform())
+                resample.SetDefaultPixelValue(img.GetPixelIDValue())
+  
+                newimage = resample.Execute(img)
+                sitk.WriteImage(newimage, end_dirs+'/Pre_N4_resam113_MR.nii')              
+
+file_name('A:/BaiduNetdiskDownload/tu xiang-zhao')   
