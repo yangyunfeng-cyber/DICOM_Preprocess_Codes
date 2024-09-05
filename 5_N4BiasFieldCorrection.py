bd41319a9b61d363fc90ebe951deb63ab6545dec
Wind&Moon <63218554+yangyunfeng-cyber@users.noreply.github.com>
2024-09-05 23:27:43 +0800

    Update and rename N4BiasFieldCorrection.py to 5_N4BiasFieldCorrection.py

--- 5_N4BiasFieldCorrection.py
+++ 5_N4BiasFieldCorrection.py
@@ -1,5 +1,10 @@
+"""
+医学图像，尤其是磁共振成像（MRI）图像，常常受到偏置场的影响，这种偏置场是由于磁场不均匀或其他因素导致的强度变化。偏置场会导致图像在不同区域的强度不均匀，从而影响图像的质量和后续的分析结果。
+N4 偏置场校正算法是 N3 偏置场校正算法的改进版本，它通过估计和校正图像的偏置场来减少这种不均匀性。该算法是一种非参数的自适应方法，基于图像的统计特性来实现校正。
+"""
+
 def file_name(file_dir):
-    """循环目录，对MR进行N4偏置场校正"""
+    """循环目录，对MRI进行N4偏置场校正"""
     N=0
     for root, dirs, files in os.walk(file_dir):             
         for path in dirs:
@@ -17,4 +22,4 @@ def file_name(file_dir):
                 N=N+1
                 print('已完成个数：', N)                        
 
-file_name('A:/BaiduNetdiskDownload/tu xiang-zhao')    
+file_name('A:/BaiduNetdiskDownload')    
